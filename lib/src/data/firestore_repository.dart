222cbefeaa42e1a9020a397fbc8bb7c2596aae9e
KristijanZic <kristijan.zic@gmail.com>
2023-12-09 19:24:08 +0100

    FirestoreRepository: created initial Firestore implementation

---
+++ lib/src/data/firestore_repository.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FirestoreRepository {
+  FirestoreRepository(this._firestore);
+  final FirebaseFirestore _firestore;
+
+  Future<void> addJob(String uid, String title, String company) async {
+    final docRef = await _firestore.collection('jobs').add({
+      // storing uid as a document field is an intentional mistake/compromise that'll be changed later
+      'uid': uid,
+      'title': title,
+      'company': company,
+    });
+    debugPrint(docRef.id);
+  }
+}
+
+final firestoreRepositoryProvider = Provider<FirestoreRepository>((ref) {
+  return FirestoreRepository(FirebaseFirestore.instance);
+});
